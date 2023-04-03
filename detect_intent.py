490b2781cbb9043d8f862eda68f41037ce4704dc
MrSabin <sabin.21011986@gmail.com>
2023-04-03 21:32:17 +0300

    Added DialogFlow learning script

--- detect_intent.py
+++ detect_intent.py
@@ -4,7 +4,6 @@ from google.cloud import dialogflow
 def detect_intent_texts(project_id, session_id, text, language_code):
     client = dialogflow.SessionsClient()
     session = client.session_path(project_id, session_id)
-
     text_input = dialogflow.TextInput(text=text, language_code=language_code)
     query_input = dialogflow.QueryInput(text=text_input)
     response = client.detect_intent(request={"session": session, "query_input": query_input})
---
+++ training.py
@@ -0,0 +1,46 @@
+import json
+
+from environs import Env
+from google.cloud import dialogflow
+
+
+def create_intent(project_id, display_name, training_phrases_parts,
+                  message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases,
+        messages=[message],
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print(f"Intent created: {response}")
+
+
+def main():
+    env = Env()
+    env.read_env()
+    project_id = env.str("PROJECT_ID")
+    with open("questions.json", "r") as questions:
+        dump = json.load(questions)
+    for group_name, phrases in dump.items():
+        questions = phrases["questions"]
+        answer = [phrases["answer"]]
+        create_intent(project_id, group_name, questions, answer)
+
+
+if __name__ == "__main__":
+    main()
