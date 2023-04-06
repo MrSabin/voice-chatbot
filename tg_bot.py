d9b1ff35362eb40ac908f4ac5deaf5afca4395df
MrSabin <sabin.21011986@gmail.com>
2023-04-06 20:14:35 +0300

    Added logging

---
+++ tg_bot.py
@@ -0,0 +1,66 @@
+import logging
+import time
+from functools import partial
+
+import telegram
+from environs import Env
+from telegram import Update
+from telegram.error import NetworkError
+from telegram.ext import (CallbackContext, CommandHandler, Filters,
+                          MessageHandler, Updater)
+
+from detect_intent import detect_intent_texts
+
+LANGUAGE_CODE = 'ru-RU'
+logger = logging.getLogger(__name__)
+
+
+class BotLogsHandler(logging.Handler):
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.bot = bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Здравствуйте!")
+
+
+def reply(update: Update, context: CallbackContext, project_id):
+    session_id = update.effective_user.id
+    try:
+        answer = detect_intent_texts(project_id, session_id, update.message.text, LANGUAGE_CODE)
+        update.message.reply_text(answer.fulfillment_text)
+    except NetworkError:
+        logger.warning("Network error, retry")
+        time.sleep(3)
+
+def main():
+    env = Env()
+    env.read_env()
+    tg_token = env.str("TG_BOT_TOKEN")
+    project_id = env.str("PROJECT_ID")
+    chat_id = env.str("TG_CHAT_ID")
+    bot = telegram.Bot(token=tg_token)
+
+    logger.setLevel(logging.INFO)
+    handler = BotLogsHandler(bot, chat_id)
+    formatter = logging.Formatter("%(process)d %(levelname)s %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.info("TG_bot started")
+
+    updater = Updater(tg_token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text, partial(reply, project_id=project_id)))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
--- vk_bot.py
+++ vk_bot.py
@@ -1,12 +1,28 @@
+import logging
+import time
 from random import randint
 
+import telegram
 import vk_api as vk
 from environs import Env
+from requests.exceptions import ConnectionError
 from vk_api.longpoll import VkEventType, VkLongPoll
 
 from detect_intent import detect_intent_texts
 
 LANGUAGE_CODE = 'ru-RU'
+logger = logging.getLogger(__name__)
+
+
+class BotLogsHandler(logging.Handler):
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.bot = bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def reply(event, vk_api, project_id):
@@ -25,10 +41,25 @@ if __name__ == "__main__":
     env = Env()
     env.read_env()
     vk_token = env.str("VK_TOKEN")
+    tg_token = env.str("TG_BOT_TOKEN")
+    chat_id = env.str("TG_CHAT_ID")
     project_id = env.str("PROJECT_ID")
+    bot = telegram.Bot(token=tg_token)
+
+    logger.setLevel(logging.INFO)
+    handler = BotLogsHandler(bot, chat_id)
+    formatter = logging.Formatter("%(process)d %(levelname)s %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.info("VK_bot started")
+
     vk_session = vk.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            reply(event, vk_api, project_id)
+        try:
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                reply(event, vk_api, project_id)
+        except ConnectionError:
+            logger.warning("Connection lost, retry")
+            time.sleep(3)
